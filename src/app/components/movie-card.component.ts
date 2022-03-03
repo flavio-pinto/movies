5b2a1847267f202306ab97cc0a67d462e345d142
Flavio Pinto <flaviopnt@hotmail.it>
2022-03-03 22:36:42 +0100

    Pulsante like aggiunto

--- src/app/components/movie-card.component.ts
+++ src/app/components/movie-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { Movie } from '../models/movie';
 
 @Component({
@@ -9,8 +8,7 @@ import { Movie } from '../models/movie';
       <div class="card" style="width: 18rem;">
         <img [src]="'http://image.tmdb.org/t/p/original' + movie.poster_path" class="card-img-top" alt="...">
         <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
+          <button type="button" class="btn"><i class="bi bi-heart-fill"></i></button>
         </div>
       </div>
     </div>
