02c7862d89f0dc6085da950ec5acca00f75c4538
Flavio Pinto <flaviopnt@hotmail.it>
2022-03-03 17:15:25 +0100

    Guard aggiunta alle rotte principali

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { MoviesPage } from './pages/movies.page';
 import { UserPage } from './pages/user.page';
 
 const routes: Routes = [
   {
+    canActivate: [AuthGuard],
     path: 'movies',
     component: MoviesPage
   },
   {
+    canActivate: [AuthGuard],
     path: 'user',
     component: UserPage
   }
